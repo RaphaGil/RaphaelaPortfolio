9297e0b1310880736a18e10d103c9c9827393897
raphaelagil <raphaela.gil@yahoo.com>
2025-06-17 16:28:04 +0100

    test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -120,7 +120,7 @@ function About() {
           </ul>
 
           <a
-            href="RaphaelaGil_FrontendEngineer.pdf"
+            href={`${process.env.PUBLIC_URL}/RaphaelaGil_FrontendEngineer.pdf`}
             download
             className="mx-2 border-2 rounded-md p-2 border-[#890465] bg-[#890465] text-white hover:text-[#890465] hover:bg-white transition-all duration-1000 ease-out transform delay-1500"
             style={{
